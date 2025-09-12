07c2234ed5e30036d1271b99343c1ee9c7c003cc
DivyaFullStackDeveloper <bondredivya12@gmail.com>
2025-09-12 13:09:38 +0530

    Add files via upload

---
+++ javaScript-practice/dictionary/actors-data.js
@@ -0,0 +1,99 @@
+// let actorData = {
+//   RohitSaraf: {
+//     Movie1: { Name: "Mismatched", ReleaseDate: "20 November 2020", Reating: 5, },
+//     Movie2: { Name: "Woh Bhi Din The", ReleaseDate: " 29 March 2024", Reating: 3.5, },
+//     Movie3: { Name: "Kamali from Nadukkaveri", ReleaseDate: " 19 February 2021", Reating: 3.5,},
+//     Movie4: { Name: "Ishq Vishk Rebound", ReleaseDate: " 21 June 2024",Reating: 5,},
+//     Movie5: { Name: "The Sky Is Pink", ReleaseDate: " 11 October 2019", Reating: 4 },
+//   },
+//  KartikAaryan: {
+//     Movie1: { Name: "Sonu Ke Titu Ki Sweety (2018)", ReleaseDate: "20 November 2020", Reating: 5, },
+//     Movie2: { Name: "Luka Chuppi (2019)", ReleaseDate: " 29 March 2024", Reating: 3.5, },
+//     Movie3: { Name: "Pati Patni Aur Woh (2019)", ReleaseDate: " 19 February 2021", Reating: 3.5,},
+//     Movie4: { Name: "Bhool Bhulaiyaa 2", ReleaseDate: " 21 June 2024",Reating: 5,},
+//     Movie5: { Name: "Satyaprem Ki Katha", ReleaseDate: " 11 October 2019", Reating: 4 },
+//   },
+
+// RanbirKapoor: {
+//     Movie1: { Name: "Saawariya", ReleaseDate: "20 November 2007", Reating: 5, },
+//     Movie2: { Name: "Woh Bhi Din The", ReleaseDate: " 29 March 2024", Reating: 3.5, },
+//     Movie3: { Name: "Kamali from Nadukkaveri", ReleaseDate: " 19 February 2021", Reating: 3.5,},
+//     Movie4: { Name: "Ishq Vishk Rebound", ReleaseDate: " 21 June 2024",Reating: 5,},
+//     Movie5: { Name: "The Sky Is Pink", ReleaseDate: " 11 October 2019", Reating: 4 },
+//   },
+//  SalmanKhan: {
+//     Movie1: { Name: "Tere Naam", ReleaseDate: "20 November 2003", Reating: 5, },
+//     Movie2: { Name: "Woh Bhi Din The", ReleaseDate: " 29 March 2024", Reating: 3.5, },
+//     Movie3: { Name: "Kamali from Nadukkaveri", ReleaseDate: " 19 February 2021", Reating: 3.5,},
+//     Movie4: { Name: "Ishq Vishk Rebound", ReleaseDate: " 21 June 2024",Reating: 5,},
+//     Movie5: { Name: "The Sky Is Pink", ReleaseDate: " 11 October 2019", Reating: 4 },
+//   },
+//    ShahrukhKhan: {
+//     Movie1: { Name: " Mohabbatein", ReleaseDate: "20 Feb 2008", Reating: 5, },
+//     Movie2: { Name: "Woh Bhi Din The", ReleaseDate: " 29 March 2024", Reating: 3.5, },
+//     Movie3: { Name: "Kamali from Nadukkaveri", ReleaseDate: " 19 February 2021", Reating: 3.5,},
+//     Movie4: { Name: "Ishq Vishk Rebound", ReleaseDate: " 21 June 2024",Reating: 5,},
+//     Movie5: { Name: "The Sky Is Pink", ReleaseDate: " 11 October 2019", Reating: 4 },
+//   },
+//    ShahrukhKhan: {
+//     Movie1: { Name: " Mohabbatein", ReleaseDate: "20 Feb 2008", Reating: 5, },
+//     Movie2: { Name: "Woh Bhi Din The", ReleaseDate: " 29 March 2024", Reating: 3.5, },
+//     Movie3: { Name: "Kamali from Nadukkaveri", ReleaseDate: " 19 February 2021", Reating: 3.5,},
+//     Movie4: { Name: "Ishq Vishk Rebound", ReleaseDate: " 21 June 2024",Reating: 5,},
+//     Movie5: { Name: "The Sky Is Pink", ReleaseDate: " 11 October 2019", Reating: 4 },
+//   },
+// };
+
+
+newData = [
+   {
+      name: "RohitSaraf",
+      movies: [
+         { Name: "Saawariya", ReleaseDate: "20 November 2007", Reating: 5, },
+         { Name: "Woh Bhi Din The", ReleaseDate: " 29 March 2024", Reating: 3.5, },
+         { Name: "Kamali from Nadukkaveri", ReleaseDate: " 19 February 2021", Reating: 3.5,},
+         { Name: "Ishq Vishk Rebound", ReleaseDate: " 21 June 2024",Reating: 5,},
+         { Name: "The Sky Is Pink", ReleaseDate: " 11 October 2019", Reating: 4 },
+      ],
+   },
+   {
+      name: " KartikAaryan",
+      movies: [
+         { Name: "Saawariya", ReleaseDate: "20 November 2007", Reating: 5, },
+         { Name: "Woh Bhi Din The", ReleaseDate: " 29 March 2024", Reating: 3.5, },
+         { Name: "Kamali from Nadukkaveri", ReleaseDate: " 19 February 2021", Reating: 3.5,},
+         { Name: "Ishq Vishk Rebound", ReleaseDate: " 21 June 2024",Reating: 5,},
+         { Name: "The Sky Is Pink", ReleaseDate: " 11 October 2019", Reating: 4 },
+      ],
+   },
+   {
+      name: "RanbirKapoor",
+      movies: [
+         { Name: "Saawariya", ReleaseDate: "20 November 2007", Reating: 5, },
+         { Name: "Woh Bhi Din The", ReleaseDate: " 29 March 2024", Reating: 3.5, },
+         { Name: "Kamali from Nadukkaveri", ReleaseDate: " 19 February 2021", Reating: 3.5,},
+         { Name: "Ishq Vishk Rebound", ReleaseDate: " 21 June 2024",Reating: 5,},
+         { Name: "The Sky Is Pink", ReleaseDate: " 11 October 2019", Reating: 4 },
+      ],
+   },
+   {
+      name: "SalmanKhan",
+      movies: [
+        { Name: "Tere Naam", ReleaseDate: "20 November 2003", Reating: 5, },
+         { Name: "Woh Bhi Din The", ReleaseDate: " 29 March 2024", Reating: 3.5, },
+         { Name: "Kamali from Nadukkaveri", ReleaseDate: " 19 February 2021", Reating: 3.5,},
+         { Name: "Ishq Vishk Rebound", ReleaseDate: " 21 June 2024",Reating: 5,},
+         { Name: "The Sky Is Pink", ReleaseDate: " 11 October 2019", Reating: 4 },
+      ],
+   },
+     {
+      name: "TigerShroff",
+      movies: [
+        { Name: "Baaghi", ReleaseDate: "20 Feb 2016", Reating: 5, },
+         { Name: "Woh Bhi Din The", ReleaseDate: " 29 March 2024", Reating: 3.5, },
+         { Name: "Kamali from Nadukkaveri", ReleaseDate: " 19 February 2021", Reating: 3.5,},
+         { Name: "Ishq Vishk Rebound", ReleaseDate: " 21 June 2024",Reating: 5,},
+         { Name: "The Sky Is Pink", ReleaseDate: " 11 October 2019", Reating: 4 },
+      ],
+   }
+]
